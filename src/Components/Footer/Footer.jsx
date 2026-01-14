88a0edade6bd3f25791b780115782ad325057eda
Shafkatahmad <shafkataahmad@gmail.com>
2026-01-15 02:24:04 +0600

    footer half done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import logo from '../../assets/logo.png';
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col items-center gap-16 bg-[#06091A] text-white'>
+      <figure>
+        <img className='w-35' src={logo} alt="" />
+      </figure>
+      <div className='max-w-330 mx-auto md:grid md:grid-cols-3 md:justify-between gap-35'>
+        <div>
+          <h4>About Us</h4>
+          <p>We are a passionate team dedicated to providing the best services to our customers.</p>
+        </div>
+        <div>
+          <h4>Quick Links</h4>
+          <ul>
+            <li>Home</li>
+            <li>Service</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Subscribe</h4>
+          <p>Subscribe to our newsletter for the latest updates.</p>
+          <div>
+            <input className='w-100 rounded-xl border border-[#131313]/15 px-7 py-4' type="email" placeholder='Enter your email' />
+          <button className='font-bold rounded-xl bg-orange-400 px-7 py-4'>Subscribe</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Footer/Newsletter.jsx
+++ src/Components/Footer/Newsletter.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const Newsletter = () => {
   return (
-    <div>
-      <h3>Subscribe to our Newsletter</h3>
-      <p>Get the latest updates and news right in your inbox!</p>
+    <div className='max-w-335 mx-auto p-6 border border-orange-300 rounded-3xl'>
+      <div className=' text-center border border-[#131313]/10 rounded-3xl'>
+      <h3 className='text-3xl font-bold mb-4'>Subscribe to our Newsletter</h3>
+      <p className='text-xl font-medium text-[#131313]/70 mb-6'>Get the latest updates and news right in your inbox!</p>
       <div>
-        <input type="email" placeholder='Enter your email' />
-        <button>Subscribe</button>
+        <input className='w-100 rounded-xl border border-[#131313]/15 px-7 py-4 mr-4' type="email" placeholder='Enter your email' />
+        <button className='font-bold rounded-xl bg-orange-400 px-7 py-4'>Subscribe</button>
       </div>
+    </div>
     </div>
   );
 };
